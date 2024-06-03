06beef07b225187ca827a8874f3f94d1a08570d4
mohmdSalah <mo.s.obidat@gmail.com>
2024-06-03 15:18:06 +0300

    deleting unnecessary lines

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const os = require('os');
 const bodyParser = require('body-parser');
 const ytdl = require('ytdl-core');
